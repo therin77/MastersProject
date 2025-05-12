c6c4062c1d111e994acff9940b3b44112898b396
therin77 <kategothberg@gmail.com>
2025-05-12 09:55:05 -0400

    working autocorr, but error with windowing size

--- test_vector_git/testautocorrelationfunction.py
+++ test_vector_git/testautocorrelationfunction.py
@@ -21,8 +21,6 @@ from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import sip
-import testautocorrelationfunction_epy_block_1 as epy_block_1  # embedded python block
 import testautocorrelationfunction_epy_block_2 as epy_block_2  # embedded python block
 
 
@@ -62,68 +60,25 @@ class testautocorrelationfunction(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
+        self.samp_rate = samp_rate = 50e6
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
-            2049, #size
-            "", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_const_sink_x_0.set_update_time(0.10)
-        self.qtgui_const_sink_x_0.set_y_axis((-2), 2)
-        self.qtgui_const_sink_x_0.set_x_axis((-2), 2)
-        self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
-        self.qtgui_const_sink_x_0.enable_autoscale(False)
-        self.qtgui_const_sink_x_0.enable_grid(False)
-        self.qtgui_const_sink_x_0.enable_axis_labels(True)
-
-
-        labels = ['', '', '', '', '',
-            '', '', '', '', '']
-        widths = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ["blue", "red", "green", "black", "cyan",
-            "magenta", "yellow", "dark red", "dark green", "dark blue"]
-        styles = [0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0]
-        markers = [0, 0, 0, 0, 0,
-            0, 0, 0, 0, 0]
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-
-        for i in range(1):
-            if len(labels[i]) == 0:
-                self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_const_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_const_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_const_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_const_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_const_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
-        self.epy_block_2 = epy_block_2.blk(window=1024)
-        self.epy_block_1 = epy_block_1.blk(additionFlag=True)
+        self.epy_block_2 = epy_block_2.blk(Px=1920, Py=1080, fv=60, samp_rate=samp_rate)
         self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.analog_sig_source_x_1 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1000, 0, (3.14*.5))
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1000, 0, 0)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, 'C:\\Users\\Kate\\OSUProject\\MastersProject\\test_file', False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 297e6, 1000, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.epy_block_1, 0))
-        self.connect((self.analog_sig_source_x_1, 0), (self.epy_block_1, 1))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle2_0, 0))
         self.connect((self.blocks_throttle2_0, 0), (self.epy_block_2, 0))
-        self.connect((self.epy_block_1, 0), (self.blocks_throttle2_0, 0))
-        self.connect((self.epy_block_2, 0), (self.qtgui_const_sink_x_0, 0))
+        self.connect((self.epy_block_2, 0), (self.blocks_file_sink_0, 0))
 
 
     def closeEvent(self, event):
@@ -140,8 +95,8 @@ class testautocorrelationfunction(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.epy_block_2.samp_rate = self.samp_rate
 
 
 
--- test_vector_git/testautocorrelationfunction_epy_block_2.py
+++ test_vector_git/testautocorrelationfunction_epy_block_2.py
@@ -2,14 +2,13 @@
 """
 Correlation/Sync Block
 Developed by Kate Gothberg
-Last Edited: 5/9/25
+Last Edited: 5/12/25
 
 """
 
 
 import numpy as np
 from gnuradio import gr
-
 
 class blk(gr.sync_block):
     
@@ -20,14 +19,17 @@ class blk(gr.sync_block):
         out_sig: output samples
         window: number of samples to correlate over
     """
-    def __init__(self, window=1.0):  
+    def __init__(self, Px=1920.0, Py= 1080.0, fv = 60.0, samp_rate = 50e6):  
         gr.sync_block.__init__(
             self,
             name='Correlation/Synch Block', 
             in_sig=[np.complex64],
             out_sig=[np.complex64]
         )
-        self.window = window
+        self.Px = Px
+        self.Py = Py
+        self.fv = fv
+        self.samp_rate = samp_rate
 
     """
     functional code over samples
@@ -36,24 +38,38 @@ class blk(gr.sync_block):
 
         samples = input_items[0] #rename input samples
         symbol_buffer = []       #storage for each block of samples
+        symbol_buffer_prev = []
+        Tv = 1/(self.fv)
+        window = 1024#np.floor(Tv * self.samp_rate)
 
         #iterate for all samples
         for i, sample in enumerate(samples):
+            #print(len(samples), window)
 
             #add to sample block
             symbol_buffer.append(samples[i])
 
             #if block is full continue processing
-            if len(symbol_buffer) > self.window:
+            if len(symbol_buffer) > window:
 
-                
-                #correlation and reset of buffer
-                corr_data = np.correlate(symbol_buffer, symbol_buffer, "full")
+                #print("in proc")
+
+                if len(symbol_buffer_prev) > window:
+                    #print("in corr")
+
+                    #correlation and reset of buffer
+                    corr_data = np.correlate(symbol_buffer, symbol_buffer_prev, "full")
+                    corr_data = corr_data / window
+
+                    print(corr_data)
+
+                    #output correlation samples
+                    for j in range(len(corr_data)-1):
+                        output_items[0][j] = corr_data[j]
+
+                    corr_data = []
+
+                symbol_buffer_prev = symbol_buffer
                 symbol_buffer = []
-
-                #output correlation samples
-                for j in range(len(corr_data)-1):
-                    print(corr_data[j])
-                    output_items[0][j] = corr_data[j]
 
         return len(output_items[0])
