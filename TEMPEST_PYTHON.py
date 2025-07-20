a2dba667d207c8ec10447e3d68e3679f8a8da247
therin77 <kategothberg@gmail.com>
2025-07-20 10:20:15 -0400

    functioning off of pregen image

--- TEMPEST_PYTHON.py
+++ TEMPEST_PYTHON.py
@@ -1,18 +1,18 @@
 ##################################
 #Master's Project OSU: gr-tempest implemntation in Python
 #Developed by: Kate Gothberg
-#Last Edited: 05/20/2025
+#Last Edited: 07/20/2025
 ##################################
 
 import uhd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import resample_poly
+from scipy.signal import resample_poly, correlate
 import PIL
 
 #for now grab samples from file, hope to implement in real-time
-f = np.fromfile(open("test_file_5M"), dtype=np.complex64)
-samples = f[1: 1000000]
+f = np.fromfile(open("test_file_50M"), dtype=np.complex64)
+samples = f[1: 1700000]
 
 """
 usrp = uhd.usrp.MultiUSRP("type=b200")
@@ -20,16 +20,18 @@ usrp.set_rx_freq(100e6)
 """
 
 fv = 60                              #frame rate
-samp_rate = 50e6 #122880              #sample rate of SDR
-Py = 1125 #1080                      #number of vert pixels + blanking
-Px = 1920 + 280 #740 #1920                       #number of hor pixels + blanking
+samp_rate = 50e6 #122880             #sample rate of SDR
+Py = 624 #628 #1125 #1080                      #number of vert pixels + blanking
+Px = 801 #1056 #1920 + 280 #740 #1920           #number of hor pixels + blanking
+py = 600
+px = 800
 fp = Py*Px*fv                        #pixel rate
 symbol_buffer = []                   #storage for each block of samples
 symbol_buffer_prev = []              #storage of previous block/ frame
 Tv = 1/(fv)                          #length of frame in time
 window = np.floor(Tv * samp_rate)    #number of samples per frame 
 M = int(.5*window)                   #default M
-del_buff = 10                        #delay length (samples)
+del_buff = 0                         #delay length (samples)
 output = []
 
 
@@ -49,7 +51,7 @@ for i, sample in enumerate(samples):
             #Autocorrelation => M
             ##################################
             
-            corr_data = np.correlate(symbol_buffer, symbol_buffer_prev, "same")
+            corr_data = correlate(symbol_buffer, symbol_buffer_prev, mode='same')
             corr_data = corr_data / window
 
             M = np.argmax(corr_data)
@@ -63,9 +65,8 @@ for i, sample in enumerate(samples):
         V = Py
         H = np.floor(Px*samp_rate/fp)
         L = V*H
-        resamp = resample_poly(symbol_buffer, L-1, M)
         
-        #output = np.append(output, resamp)
+        resamp = resample_poly(symbol_buffer, L, M)
             
         #reset symbol_buffer and allocate prev buffer
         symbol_buffer_prev = symbol_buffer
@@ -85,8 +86,26 @@ for i, sample in enumerate(samples):
         np.append(real, zeros)
         
         ##################################
-        #Delay
+        #Normalize
         ##################################
+        gain = 256/np.max(real)
+        amp_real = real*gain
+
+        ##################################
+        #Float to Short
+        ##################################
+
+        ##################################
+        #Display
+        ##################################
+    
+        rows = int(V)
+        cols = int(np.floor(len(amp_real)/V))
+        del_len = len(amp_real) - rows*cols
+        amp_real = amp_real[:-del_len]
+        matrix = np.reshape(amp_real,(rows,cols))
+        screen = PIL.Image.fromarray(matrix)
+        screen.show()
         
 
 """
